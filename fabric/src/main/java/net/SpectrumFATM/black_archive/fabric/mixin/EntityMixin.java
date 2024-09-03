1b9a555a44bf63145edd2bac2b72ff042ede530d
EnderTrekkie <63455321+EnderTrekkie@users.noreply.github.com>
2024-09-03 15:55:13 -0700

    ZeroGrav fixed
    change 1: now resets prevMotion when player is on ground. Now the player doesn't drop when they step off a ledge.
    change 2: has a more tolerant check for vertical velocity. Rounding errors can make certain situations not be accounted for.

--- fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
+++ fabric/src/main/java/net/SpectrumFATM/black_archive/fabric/mixin/EntityMixin.java
@@ -1,13 +1,16 @@
 package net.SpectrumFATM.black_archive.fabric.mixin;
 
+import net.SpectrumFATM.black_archive.fabric.BlackArchive;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.block.Block;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,7 +26,11 @@ public abstract class EntityMixin {
     private boolean wasOnGround;
 
     @Inject(method = "tick", at = @At("HEAD"))
-    private void applyZeroGravity(CallbackInfo info) {
+    private void tick(CallbackInfo info) {
+        applyZeroGravity();
+    }
+
+    private void applyZeroGravity() {
         Entity entity = (Entity) (Object) this;
         World world = entity.getWorld();
 
@@ -32,8 +39,16 @@ public abstract class EntityMixin {
             return;
         }
 
+        BlackArchive.LOGGER.info(String.valueOf(entity.getVelocity().y - this.prevMotionY));
+        BlackArchive.LOGGER.info(String.valueOf(entity.getVelocity().y));
+        BlackArchive.LOGGER.info(String.valueOf(this.prevMotionY));
+
         // Skip if entity is on the ground
         if (!isInFreefall(entity)) {
+            //update previous motion, otherwise it could be invalid after the player steps off the ground
+            this.prevMotionX = 0;
+            this.prevMotionY = 0;
+            this.prevMotionZ = 0;
             return;
         }
 
@@ -74,9 +89,9 @@ public abstract class EntityMixin {
         //reverse drag. doing it before everything makes the math easier.
         //minecraft applies a small drag by multiplying the motion by 0.91. if we divide the motion by 0.91, the velocity is back to where it should be
         entity.setVelocity(
-            entity.getVelocity.x / 0.91F;
-            entity.getVelocity.y / 0.9800000190734863D;
-            entity.getVelocity.z / 0.91F;
+                entity.getVelocity().getX() / 0.91F,
+                entity.getVelocity().y / 0.9800000190734863D,
+                entity.getVelocity().z / 0.91F
         );//i have no clue why galacticraft reversed vertical drag using the above number, but they must be right
 
         // get difference between velocity last frame and this frame.
@@ -87,8 +102,8 @@ public abstract class EntityMixin {
 
         //if the acceleration in the y direction is equal to gravity, set it to zero
         //i worry that this might cause any slight vertical acceleration to cause the player to drop.
-        if(deltaY == 0.08){//minecraft accelerates entities by 0.08 blocks per tick per tick. This means that deltaY changes by 0.08 each tick.
-            deltaY = 0;//we're in space, so accelerate by 0 blocks per tick per tick.
+        if(deltaY - -0.08 < 0.000001){//minecraft accelerates entities by 0.08 blocks per tick per tick. This means that deltaY changes by 0.08 each tick
+            deltaY = 0.0;//we're in space, so accelerate by 0 blocks per tick per tick.
         }
 
         //give the updated velocity back to the entity
@@ -99,11 +114,11 @@ public abstract class EntityMixin {
         );
 
         // Limit speed to avoid infinite acceleration. this is just so the player doesn't fly out of loaded chunks uncontrollably
-        float speedLimit = 0.7F;
+        float speedLimit = 0.1f;
         entity.setVelocity(
-            MathHelper.max(MathHelper.min(entity.getVelocity.x,speedLimit),-speedLimit),
-            MathHelper.max(MathHelper.min(entity.getVelocity.y,speedLimit),-speedLimit),
-            MathHelper.max(MathHelper.min(entity.getVelocity.z,speedLimit),-speedLimit)
-            );
+                Math.max(Math.min(entity.getVelocity().x,speedLimit),-speedLimit),
+                Math.max(Math.min(entity.getVelocity().y,speedLimit),-speedLimit),
+                Math.max(Math.min(entity.getVelocity().z,speedLimit),-speedLimit)
+        );
     }
 }
